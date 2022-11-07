8cca3164bbd9bb5ff1193a16e799f03bb7c70aab
Siddharth <sid.sharma1704@gmail.com>
2022-11-07 14:18:00 +0530

    Added Landing page

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,19 +7,19 @@ export default function Navbar(){
     const router = useRouter();
 
     return(
-        <div className="lg:flex justify-between lg:w-9/12 py-10 mx-auto font-Bebas text-xl">
+        <div className="lg:flex justify-between lg:w-9/12 py-10 mx-auto font-Bebas lg:text-xl text-center lg:text-left">
             <div className=""><p className="text-4xl font-semibold">RETREND</p>Back to 80s</div>
             <div>
-                <ul className="lg:flex justify-evenly">
-                    <Link href={'/'}><li className={router.asPath === '/' ? "px-8 underline font-semibold" : "px-8"}>Home</li></Link>
-                    <Link href={'/dashboard'}><li className={router.asPath === '/dashboard' ? "px-8 underline font-semibold" : "px-8"}>Dashboard</li></Link>
-                    <Link href={'/redeem'}><li className={router.asPath === '/redeem' ? "px-8 underline font-semibold" : "px-8"}>Redeem</li></Link>
-                    <Link href={'/features'}><li className={router.asPath === '/features' ? "px-8 underline font-semibold" : "px-8"}>Features</li></Link>
-                    <Link href={'/gamebar'}><li className={router.asPath === '/gamebar' ? "px-8 underline font-semibold" : "px-8"}>Game Bar</li></Link>
+                <ul className="flex justify-evenly">
+                    <Link href={'/'}><li className={router.asPath === '/' ? "lg:px-8 underline font-semibold" : "lg:px-8"}>Home</li></Link>
+                    <Link href={'/dashboard'}><li className={router.asPath === '/dashboard' ? "lg:px-8 underline font-semibold" : "lg:px-8"}>Dashboard</li></Link>
+                    <Link href={'/redeem'}><li className={router.asPath === '/redeem' ? "lg:px-8 underline font-semibold" : "lg:px-8"}>Redeem</li></Link>
+                    <Link href={'/features'}><li className={router.asPath === '/features' ? "lg:px-8 underline font-semibold" : "lg:px-8"}>Features</li></Link>
+                    <Link href={'/gamebar'}><li className={router.asPath === '/gamebar' ? "lg:px-8 underline font-semibold" : "lg:px-8"}>Game Bar</li></Link>
                 </ul>
             </div>
             <div className="">
-                <p className="bg-gradient-to-r pt-1 from-blue-500 to-pink-600 px-5 rounded-sm text-gray-200">Login</p>
+                <p className="bg-gradient-to-r pt-1 from-blue-500 to-pink-600 px-5 rounded-sm text-gray-200 w-fit mx-auto">Login</p>
             </div>
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,38 @@
 import React from "react"
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className="text-gray-900 w-screen h-screen">
       <img src="./wave-bg.png" className="absolute top-0 -z-10 h-screen"/>
       <Navbar />
+
+      <div>
+        <div className="landing-section">
+          <p className="lg:text-7xl text-3xl font-Bungee text-center text-gray-200">STAY TRENDY WITH<br></br>RETRO STYLE FUN</p>
+        </div>
+        <div className="lg:w-6/12 font-Bebas text-gray-900  text-lg mx-auto glassmorph my-10">
+          <p className="font-Bungee text-2xl text-center">Play.Earn.Redeem</p>
+          <p className="p-2">Welcome to <span className="text-blue-900 underline">ReTrend</span>, play awesome games built almost half a decade ago, and get rewarded with our new <span className="text-pink-900 underline">digital coins</span>. Redeem your coins and get <span className="text-orange-900 underline">Premium stuffs</span> for different games.<br />Tired of playing online? We set up <span className="text-teal-900 underline">Public Outlets</span> too, don't waste a moment and visit your nearest one.</p>
+          <p className="font-Bungee text-center text-2xl">#We Set The Trend</p>
+        </div>
+        <div className="text-center">
+          <Link href={'/gamebar'}>
+            <button class="relative font-Bebas text-2xl inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-blue-500 group-hover:from-pink-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none">
+              <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                  Play Now!
+              </span>
+            </button>
+          </Link>
+        </div>
+        <div className="invisible lg:visible flex justify-end">
+          <img src="./car.gif" className="absolute bottom-10 right-10"/>
+        </div>
+      </div>
+
+
       <Footer />
     </div>
   )
