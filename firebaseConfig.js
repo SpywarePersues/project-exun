0be668b6870fc0afb13b38fbe773d8091359fdf8
SpywarePersues <spywarepersues284@gmail.com>
2022-11-07 23:52:59 +0530

    Firebase change

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBz4HKLGx92URmryYziBUZcOAY5cesOjn8",
-  authDomain: "test-f44e0.firebaseapp.com",
-  projectId: "test-f44e0",
-  storageBucket: "test-f44e0.appspot.com",
-  messagingSenderId: "881274645506",
-  appId: "1:881274645506:web:151f4a7bec2e2a62fe8b1b"
+  apiKey: "AIzaSyC2Ul0YoOauUm0R_fI7ZLyCuz-8svDmv00",
+  authDomain: "dummy-exun.firebaseapp.com",
+  projectId: "dummy-exun",
+  storageBucket: "dummy-exun.appspot.com",
+  messagingSenderId: "111123256030",
+  appId: "1:111123256030:web:2868d14eb433133cf4e84f",
+  measurementId: "G-V2MNY4Z4KF"
 };
 
 export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
