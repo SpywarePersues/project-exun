dd2d3c264c21a2e1936c1e345bc34655a9e284ac
Siddharth <sid.sharma1704@gmail.com>
2022-11-08 11:31:29 +0530

    fix

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC2Ul0YoOauUm0R_fI7ZLyCuz-8svDmv00",
-  authDomain: "dummy-exun.firebaseapp.com",
-  projectId: "dummy-exun",
-  storageBucket: "dummy-exun.appspot.com",
-  messagingSenderId: "111123256030",
-  appId: "1:111123256030:web:2868d14eb433133cf4e84f",
-  measurementId: "G-V2MNY4Z4KF"
+  apiKey: "AIzaSyCbiq9bS6jjgguW6RuwdgdOnb9PdRqRJBM",
+  authDomain: "exun-6dd56.firebaseapp.com",
+  projectId: "exun-6dd56",
+  storageBucket: "exun-6dd56.appspot.com",
+  messagingSenderId: "1032989169042",
+  appId: "1:1032989169042:web:83eb8c493f4c4df2a51d4b",
+  measurementId: "G-K94VB3J670"
 };
 
 export const app = initializeApp(firebaseConfig);
