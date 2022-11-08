fd1105c5e12946b51d2c8364b341cc6d9192736f
SpywarePersues <spywarepersues284@gmail.com>
2022-11-08 12:58:00 +0530

    Dashboard ui changes

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -48,30 +48,27 @@ export default function Dashboard(){
                 mounted ? 
                 token ? 
                 <div className="lg:flex mx-2 lg:mx-5 font-Inter">
-                    <div className="glassmorph lg:w-3/12 py-5 pl-5">
+                    <div className="glassmorph lg:w-6/12 py-5 pl-5 mx-auto my-10">
                         <div className="lg:flex">
-                            <img src={localStorage.getItem('PFP')} className="w-28 mx-auto lg:mx-0 rounded-full lg:rounded-lg"/>
+                            <img src={localStorage.getItem('PFP')} className="w-3/12 mx-auto lg:mx-0 rounded-full lg:rounded-lg"/>
                             <div className="lg:px-5 px-2">
-                                {localStorage.getItem('Name')}
-                                <br></br>
-                                {localStorage.getItem('Email')}
-                                <br></br>
-                                Balance: <img src="./coin.gif" className="w-5 inline-flex"/>{userBalance}
-                                <br></br>
-                                Rank: {localStorage.getItem('Rank')}
+                                <h1 className="font-Bebas text-5xl text-white mt-4">{localStorage.getItem('Name')}</h1>
+                                <h1 className="font-Bebas text-2xl text-white mb-4 mt-1">{localStorage.getItem('Email')}</h1>
+                                <h1 className="font-Bebas text-xl text-white mt-1">Balance: <img src="./coin.gif" className="w-5 inline-flex"/> {userBalance}</h1>
+                                <h1 className="font-Bebas text-xl text-white mt-1">Rank: {localStorage.getItem('Rank')}</h1>
                             </div>
                         </div>
                         <div className="pt-10">
-                            <p className="text-center font-Bebas text-3xl">Purchases</p>
+                            <p className="text-center font-Bebas text-5xl text-white mb-6">Purchases</p>
                             <div className="lg:h-64 lg:overflow-y-scroll purchases overflow-x-hidden" id="style3">
                                 {
                                     userPurchases.map((data) => {
                                         return(
                                         <div className="flex py-2" key={data.price}>
-                                            <img src={data.image} className="w-16 h-12" />
+                                            <img src={data.image} className="w-32 h-22 rounded-md" />
                                             <div className="px-2">
-                                                <p>Name: {data.item}</p>
-                                                <p>Bought for: {data.price}</p>
+                                                <p className="font-Bebas text-3xl text-white">Name: {data.item}</p>
+                                                <p className="font-Bebas text-lg text-white">Bought for: <img src="./coin.gif" className="w-5 inline-flex"/> {data.price}</p>
                                             </div>
                                         </div>
                                     )})
@@ -85,6 +82,7 @@ export default function Dashboard(){
                 : 
                 <div>Loading...</div>
             }
+            <Footer />
         </div>
     )
 }
