9e20540460455861f645ebd9addf38eade5f296c
SpywarePersues <spywarepersues284@gmail.com>
2022-11-08 20:49:13 +0530

    Minor change in gamebar

--- pages/gamebar.js
+++ pages/gamebar.js
@@ -7,8 +7,8 @@ export default function GameBar(){
         <div>
             <Navbar title="Game Bar" />
             <img src="./wave-bg.png" alt="wave" className="fixed top-0 -z-10 h-screen"/>
-            <div className="games grid grid-cols-4 lg:w-11/12 mx-auto">
-                <a href={'https://snakemaster.vercel.app/'} target="_Blank" rel="noopener">
+            <div className="games grid grid-cols-4 lg:w-11/12 mx-auto text-white">
+                <a href={'https://snakemaster.vercel.app/'} target="_Blank" rel="noreferrer">
                 <div className="glassmorph hover:scale-105 m-2 transition-transform ease-in-out cursor-pointer text-center text-2xl font-Bebas">
                     <img src="https://www.coolmathgames.com/sites/default/files/snake.png" className="p-2 h-48 w-full" />
                     Play Now {`[BETA]`}
@@ -29,10 +29,6 @@ export default function GameBar(){
                 <div className="glassmorph hover:scale-105 m-2 transition-transform ease-in-out cursor-pointer text-center text-2xl font-Bebas">
                     <img src="https://wallpaper.dog/large/20441629.jpg" className="p-2 h-48 w-full" />
                     Under maintenance
-                </div>
-                <div className="glassmorph hover:scale-105 m-2 transition-transform ease-in-out cursor-pointer text-center text-2xl font-Bebas">
-                    <img src="https://media.istockphoto.com/vectors/red-rubber-stamp-icon-on-transparent-background-vector-id919303478?k=20&m=919303478&s=170667a&w=0&h=ZSA21qvRNTWeTUSTzwrZdYwdzf9VEPD2k8UE8_Y4--M=" className="p-2 h-48 w-full" />
-                    More Games Coming soon
                 </div>
             </div>
         </div>
