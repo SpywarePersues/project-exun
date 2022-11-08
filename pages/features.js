bc607ebf56ed98f0d99a790ada558a434072d469
SpywarePersues <spywarepersues284@gmail.com>
2022-11-08 18:30:22 +0530

    Added features page

--- pages/features.js
+++ pages/features.js
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 export default function Features(){
@@ -5,6 +6,16 @@ export default function Features(){
         <div>
             <Navbar title="Features" />
             <img src="./wave-bg.png" alt="wave" className="fixed top-0 -z-10 h-screen"/>
+            <h1 className='text-7xl font-Bungee text-center pb-5 pt-5 font-semibold'>Features</h1>
+            <div className="lg:flex glassmorph w-10/12 mx-auto">
+                <img src="https://cdn.discordapp.com/attachments/910730837996224584/1039507906841825320/image.png" className=" my-6 w-11/12 mx-auto lg:mx-4 lg:w-7/12 rounded-md" />
+                <h1 className="text-white font-Bebas text-2xl lg:text-4xl xl:text-5xl mx-auto text-center lg:my-auto my-4">Amazing User Experience.</h1>
+            </div>
+            <div className="lg:flex glassmorph w-10/12 mx-auto my-10">
+                <h1 className="text-white font-Bebas text-2xl lg:text-4xl xl:text-5xl mx-auto text-center lg:my-auto my-4">Amazing User Experience.</h1>
+                <img src="https://cdn.discordapp.com/attachments/910730837996224584/1039507906841825320/image.png" className=" my-6 w-11/12 mx-auto lg:mx-4 lg:w-7/12 rounded-md" />
+            </div>
+            <Footer />
         </div>
     )
 }
